b3e70ba8e873fbf0344d696b139baa655bb7f839
Rajaramesh-polumuru <rajaramesh.polumuru@gmail.com>
2020-12-30 17:31:23 +0530

    deploy

---
+++ src/components/TodoForm.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect, useRef } from "react";
+
+function TodoForm(props) {
+  const [input, setInput] = useState("");
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
+    setInput("");
+  };
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Things To Do"
+        value={input}
+        name="text"
+        className="todo-input"
+        onChange={handleChange}
+        ref={inputRef}
+        autoComplete="off"
+      />
+      <button className="todo-button">Add</button>
+    </form>
+  );
+}
+
+export default TodoForm;
